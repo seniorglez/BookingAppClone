a199be203fd44b144d99eed1ce533ae32a12e5c7
Diego Domínguez <d.dominguez.glez1111@gmail.com>
2020-01-29 18:06:57 +0100

    implemented titles and some data bindings

--- BookingAppClone/Feed.xaml.cs
+++ BookingAppClone/Feed.xaml.cs
@@ -14,7 +14,22 @@ namespace BookingAppClone
 
         private async void SearchButton_Clicked(System.Object sender, System.EventArgs e)
         {
-            await Navigation.PushAsync(new SearchResults());
+            SearchModel sm = new SearchModel {
+                City=CityEntry.Text,
+                Date1=DatePikerA.Date.ToShortDateString(),
+                Date2 = DatePikerB.Date.ToShortDateString()
+            };
+            SearchResults sr = new SearchResults();
+            sr.BindingContext = sm;
+            await Navigation.PushAsync(sr);
         }
+    }
+
+    internal class SearchModel
+    {
+        public String City { get; set; }
+        public String Date1 { get; set; }
+        public String Date2 { get; set; }
+        public String Habitaciones { get; set; }
     }
 }
