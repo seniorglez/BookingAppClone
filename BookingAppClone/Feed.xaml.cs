5466b08a0c69ed60d97fa14e8a717350fd82baf0
Diego Domínguez <d.dominguez.glez1111@gmail.com>
2020-01-30 18:40:43 +0100

    now SALOJAMIENTO have his own class named SearchTab

--- BookingAppClone/Feed.xaml.cs
+++ BookingAppClone/Feed.xaml.cs
@@ -11,25 +11,7 @@ namespace BookingAppClone
         {
             InitializeComponent();
         }
-
-        private async void SearchButton_Clicked(System.Object sender, System.EventArgs e)
-        {
-            SearchModel sm = new SearchModel {
-                City=CityEntry.Text,
-                Date1=DatePikerA.Date.ToShortDateString(),
-                Date2 = DatePikerB.Date.ToShortDateString()
-            };
-            SearchResults sr = new SearchResults();
-            sr.BindingContext = sm;
-            await Navigation.PushAsync(sr);
-        }
     }
 
-    internal class SearchModel
-    {
-        public String City { get; set; }
-        public String Date1 { get; set; }
-        public String Date2 { get; set; }
-        public String Habitaciones { get; set; }
-    }
+    
 }
---
+++ BookingAppClone/SearchTab.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BookingAppClone
+{
+    public partial class SearchTab : ContentPage
+    {
+        public SearchTab()
+        {
+            InitializeComponent();
+        }
+
+        private async void SearchButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            SearchModel sm = new SearchModel
+            {
+                City = CityEntry.Text,
+                Date1 = DatePikerA.Date.ToShortDateString(),
+                Date2 = DatePikerB.Date.ToShortDateString()
+            };
+            SearchResults sr = new SearchResults();
+            sr.BindingContext = sm;
+            await Navigation.PushAsync(sr);
+        }
+    }
+    internal class SearchModel
+    {
+        public String City { get; set; }
+        public String Date1 { get; set; }
+        public String Date2 { get; set; }
+        public String Habitaciones { get; set; }
+    }
+}
